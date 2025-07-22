8f11b57b6f0134e0e65424a3dcf0e7fe11006941
snowa-snowa <sione8172635@gmail.com>
2025-07-22 10:31:47 +0500

    we canceled http

--- DJ_Porfolio_Render/settings.py
+++ DJ_Porfolio_Render/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mx4x^6+#ovfq+euyds$&k$yvl7wvx#=j(05-(sp2&mgq1)z5i@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["portfolio-c4pm.onrender.com"]
+ALLOWED_HOSTS = ["portfolio-c4pm.onrender.com", "127.0.0.1"]
 
 
 # Application definition
